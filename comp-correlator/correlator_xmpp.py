e8a4fd7c182f570557e36f741291941e1f3bd607
Juan <callesjuan@gmail.com>
2015-04-02 17:04:36 -0300

    correlator is now suggesting the pivot group to all the other groups inside a super group

--- comp-correlator/correlator_xmpp.py
+++ comp-correlator/correlator_xmpp.py
@@ -1,4 +1,4 @@
-import datetime, dateutil.parser, logging, signal, sys, time
+import datetime, dateutil.parser, signal, sys, time
 import json, numpy as np, pymongo, sklearn.cluster as skc, sleekxmpp
 
 class CorrelatorXMPP(sleekxmpp.ClientXMPP):
@@ -13,6 +13,10 @@ class CorrelatorXMPP(sleekxmpp.ClientXMPP):
     self.register_plugin('xep_0203') # Delayed delivery
     self.add_event_handler("session_start", self.handle_start)
     self.add_event_handler("message", self.handle_message)
+    
+    jid_tuple = jid.split("@")
+    self.DOMAIN = jid_tuple[1]
+    self.MUC_JID = 'conference.' + self.DOMAIN
 
     self.local_sources = {}
     self.local_groups = {}
@@ -25,13 +29,11 @@ class CorrelatorXMPP(sleekxmpp.ClientXMPP):
       sys.exit(0)
 
   def handle_start(self, event):
-    logging.info("connected")
     print "connected"
-    self.get_roster()
     self.send_presence(pstatus="")
+    self.get_roster()
 
   def handle_message(self, message):
-    logging.info(message)
     print message
     delay = message["delay"].get_stamp()
     if delay is None: # process only if it isn't an offline message
@@ -42,7 +44,7 @@ class CorrelatorXMPP(sleekxmpp.ClientXMPP):
         head = {'from':str(message["from"]), 'to':str(message["to"])} # header arguments
         func(head, args)
       except:
-        logging.warning("message error")
+        print "message error"
     # self.make_message(mto=message["from"], mbody=message["body"]).send()
 
   def handle_func(self, str_func):
@@ -65,29 +67,33 @@ class CorrelatorXMPP(sleekxmpp.ClientXMPP):
   ROUTINES/TASKS
   '''
   def calculate_centroids(self):
+    print "calculating centroids"
     # calculate group centroids
-    groups = self.db['groups'].find()
+    groups = self.db['groups'].find({'status':'active'})
     for group in groups:
       print group['group_jid']
-      members = self.db['sources'].find({'group':group['group_jid']})
-      sum_lng = 0
-      sum_lat = 0
-      count = len(group['members'])
-      print "count " + str(count) + " " + str(members.count())
-      for member in members:
-        print member['latlng']
-        latlng = member['latlng']
-        sum_lng += latlng[0]
-        sum_lat += latlng[1]
-      centroid_lng = sum_lng/count
-      centroid_lat = sum_lat/count
-      group_latlng = str(centroid_lat) + "," + str(centroid_lng)
-      self.db['groups'].update({'group_jid':group['group_jid']}, {'$set' : {'latlng' : group_latlng}})
+      members = self.db['sources'].find({'status':'streaming', 'group':group['group_jid']})
+      if members.count() > 0:
+        sum_lng = 0
+        sum_lat = 0
+        count = len(group['members'])
+        print "count " + str(count) + " " + str(members.count())
+        for member in members:
+          print member['latlng']
+          latlng = member['latlng']
+          sum_lng += latlng[0]
+          sum_lat += latlng[1]
+        centroid_lng = sum_lng/count
+        centroid_lat = sum_lat/count
+        group_latlng = str(centroid_lat) + "," + str(centroid_lng)
+        self.db['groups'].update({'group_jid':group['group_jid']}, {'$set' : {'centroid' : group_latlng}})
+    print "calculated centroids"
   
   def send_suggestions(self):
+    print "sending suggestions"
     # cluster groups (group location, centroid, should be updated after its sources send update_location)
     # if it finds clusters then checks which group is more crowded and ancient, the pivot/master, finally it suggests members from the remaining groups to join it
-    groups_iter = self.db['groups'].find()
+    groups_iter = self.db['groups'].find({'status':'active'})
     groups_arr = []
     X_arr = []
     
@@ -95,7 +101,7 @@ class CorrelatorXMPP(sleekxmpp.ClientXMPP):
 
     i = 0
     for group in groups_iter:
-      latlng = group['latlng']
+      latlng = group['centroid']
       groups_arr.append(group)
       new_row = [i, float(latlng[0]), float(latlng[1])]
       X_arr.append(new_row)
@@ -118,10 +124,15 @@ class CorrelatorXMPP(sleekxmpp.ClientXMPP):
         super_groups[sg_key]['members'] = []
         super_groups[sg_key]['pivot'] = i
       else:
-        stamp_pivot = dateutil.parser.parse(groups_arr[super_groups[sg_key]['pivot']]['stamp'])
-        stamp_curr = dateutil.parser.parse(groups_arr[i]['stamp'])
-        if stamp_curr < stamp_pivot:
+        members_pivot = len(groups_arr[super_groups[sg_key]['pivot']]['members'])
+        members_curr = len(groups_arr[i]['members'])
+        if members_curr > members_pivot:
           super_groups[sg_key]['pivot'] = i
+        elif members_curr == members_pivot:      
+          stamp_pivot = dateutil.parser.parse(groups_arr[super_groups[sg_key]['pivot']]['stamp'])
+          stamp_curr = dateutil.parser.parse(groups_arr[i]['stamp'])
+          if stamp_curr < stamp_pivot:
+            super_groups[sg_key]['pivot'] = i
       super_groups[sg_key]['members'].append(X_arr[i])
     
     # print super_groups
@@ -130,7 +141,28 @@ class CorrelatorXMPP(sleekxmpp.ClientXMPP):
       for i in range(0, len(sg['members'])):
         g = sg['members'][i][0]
         if sg['pivot'] != g:
-          print "suggest sources at [" + groups_arr[g]['group_jid'] + "] to join [" + groups_arr[sg['pivot']]['group_jid'] + "]"
+          notify_jid = (groups_arr[g]['group_jid']+"@"+self.MUC_JID)
+          pivot = groups_arr[sg['pivot']]['group_jid']
+
+          print "suggest sources at [" + notify_jid + "] to join [" + pivot + "]"
+          
+          msg = {
+            'func':'notify_correlation',
+            'args': {
+              'group_jid':pivot,
+              'hashtags':groups_arr[sg['pivot']]['hashtags'],
+              'num_members':len(groups_arr[sg['pivot']]['members'])
+            }
+          }
+
+          try:
+            self.make_presence(pto=str(notify_jid+"/correlator")).send()
+            self.make_message(mto=notify_jid, mbody=json.dumps(msg), mtype='groupchat').send()
+            self.make_presence(ptype='unavailable', pto=notify_jid).send()
+          except:
+            print sys.exc_info()
+
+    print "sended suggestions"
       
 
 '''
@@ -146,19 +178,16 @@ client = None
 def main(argv):
   signal.signal(signal.SIGINT, signal_handler)
   
-  logging.basicConfig(filename='correlator_xmpp.log',level=logging.DEBUG)
-  logging.info("correlator started")
-  
   global client
 
-  jid = "correlator@juancalles.ddns.net"
+  jid = "correlator@git.lprm.inf.ufes.br"
   pwd = "123"
   mongo_uri = "mongodb://localhost:27017"
 
   client = CorrelatorXMPP(jid, pwd, mongo_uri)
   client.connect()
   client.process()
-
+  
   while True:
     try:
       # msg_in = raw_input("> ")
